fe1f9fcc0d217782c1fe768879b48a2031000c15
clauddy <claudemakarova@gmail.com>
2020-02-19 10:58:16 +0300

    users

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,14 +1,10 @@
 <template>
   <div class="user-view">
     <template v-if="user">
-      <h1>User : {{ user.id }}</h1>
-      <p>Created {{ user.created | getTime }} ago</p>
+      <h2>{{ user.username }}</h2>
+      <p>Created at {{ user.created_at}} ago</p>
       <p>Karma: {{ user.karma }}</p>
       <p v-html="user.about">{{ user.about }}</p>
-      <p class="links">
-        <a :href="'https://news.ycombinator.com/submitted?id=' + user.id" target="_blank">submissions</a> |
-        <a :href="'https://news.ycombinator.com/threads?id=' + user.id" target="_blank">comments</a>
-      </p>
     </template>
     <template v-else-if="user === false">
       <h1>User not found.</h1>
@@ -27,15 +23,11 @@
       }
     },
     created: function() {
-      axios.get("https://hacker-news.firebaseio.com/v0/user/" + this.$route.params.id + ".json")
-        .then(res => {
-          this.user = res.data;
-          this.user.forEach(id => {
-            axios.get("https://hacker-news.firebaseio.com/v0/user/" + id + ".json")
-              .then(res => { this.user.push(res.data); });
-          })
-        })
-    }
+      axios.get("http://hn.algolia.com/api/v1/users/" + this.$route.params.id)
+              .then(response => {
+                this.user = response.data;
+              })
+    },
   }
 </script>
 
