c6d12af6ff6a8c264d723c48d383853be10363b6
clauddy <claudemakarova@gmail.com>
2020-02-19 11:18:17 +0300

    comments

--- src/components/User.vue
+++ src/components/User.vue
@@ -5,6 +5,16 @@
       <p>Created at {{ user.created_at}} ago</p>
       <p>Karma: {{ user.karma }}</p>
       <p v-html="user.about">{{ user.about }}</p>
+      <div v-for="user in comments.hits" :key="user.objectID">
+        <div class="comment-wrap">
+          <div class="comment-block">
+            <div class="comment-text" v-html="user.comment_text">{{user.comment_text}}</div>
+            <div class="bottom-comment">
+              <div class="date">{{ user.created_at }} </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </template>
     <template v-else-if="user === false">
       <h1>User not found.</h1>
@@ -20,13 +30,19 @@
     data: function() {
       return {
         user: {},
+        comments: []
       }
     },
     created: function() {
       axios.get("http://hn.algolia.com/api/v1/users/" + this.$route.params.id)
-              .then(response => {
-                this.user = response.data;
-              })
+        .then(response => {
+          this.user = response.data;
+        });
+
+      axios.get("https://hn.algolia.com/api/v1/search?tags=author_" + this.$route.params.id + ",(comment)")
+        .then(response => {
+          this.comments = response.data;
+        });
     },
   }
 </script>
