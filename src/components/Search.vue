75f4fbc7f849e4b46a688f6e86e856422b26a8bc
Olga Makarova <olgamakarovatvr@gmail.com>
2020-02-20 18:47:25 +0300

    add hooks, fix search

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>SEARCH</h1>
-    <Item v-for="story in stories.hits" :key="story.id" :story="story"></Item>
+    <Item v-for="item in items.hits" :key="item.id" :story="item"></Item>
   </div>
 </template>
 
@@ -13,16 +13,32 @@
     name: "Search",
     data: function() {
       return {
-        stories: [],
+        items: [],
       };
     },
     components: {
         'Item': Item
     },
+    mounted() {
+        this.items = this.getItems(this.id)
+    },
+    beforeRouteUpdate ( to, from , next ) {
+        this.items = this.getItems(to.params.id);
+        next();
+    },
+    methods: {
+      getItems(id) {
+        axios.get("http://hn.algolia.com/api/v1/search?query=" +  id + "&tags=story")
+          .then(response => {
+            this.items = response.data;
+          })
+        return this.items;
+      }
+    },
     created: function() {
-      axios.get("http://hn.algolia.com/api/v1/search?query=" +this.$route.params.id  + "&tags=story")
+      axios.get("http://hn.algolia.com/api/v1/search?query=" + this.$route.params.id  + "&tags=story")
         .then(response => {
-          this.stories = response.data;
+          this.items = response.data;
         })
     },
   }
