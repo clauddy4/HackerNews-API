2ef7edebc30c49d28e41fecaadccf9542fa4baac
Olga Makarova <olgamakarovatvr@gmail.com>
2020-02-21 21:52:57 +0300

    fix timeline

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -9,9 +9,20 @@ export function getTime (time) {
     }
 }
 
+export function toNormalTime (time) {
+    return time.replace('T', ' ').replace('.000Z', '')
+}
+
 function pluralize (time, label) {
     if (time === 1) {
         return time + label
     }
     return time + label + 's'
+}
+
+export function host (url) {
+    const host = url.replace(/^https?:\/\//, '').replace(/\/.*$/, '');
+    const parts = host.split('.').slice(-3);
+    if(parts[0] === 'www') parts.shift();
+    return parts.join('.');
 }
