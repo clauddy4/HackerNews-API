fc5f9a9f967d327ae6c24e3a4c8e6259554c23ad
Olga Makarova <olgamakarovatvr@gmail.com>
2020-02-22 15:38:42 +0300

    fix utils

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,23 +1,5 @@
-export function getTime (time) {
-    const between = Date.now() / 1000 - Number(time)
-    if (between < 3600) {
-        return pluralize(~~(between / 60), ' minute')
-    } else if (between < 86400) {
-        return pluralize(~~(between / 3600), ' hour')
-    } else {
-        return pluralize(~~(between / 86400), ' day')
-    }
-}
-
 export function toNormalTime (time) {
-    return time.replace('T', ' ').replace('.000Z', '')
-}
-
-function pluralize (time, label) {
-    if (time === 1) {
-        return time + label
-    }
-    return time + label + 's'
+    if (time) return time.replace('T', ' ').replace('.000Z', '')
 }
 
 export function host (url) {
